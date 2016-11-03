5ba5fa9c1400cacea58e62887adbb5254ca0567b
nolachen <nolachen@gmail.com>
2016-11-02 20:22:58 -0400

    view 1

---
+++ src/cs3500/music/view/MidiViewImpl.java
@@ -0,0 +1,59 @@
+package cs3500.music.view;
+
+import javax.sound.midi.*;
+
+/**
+ * A skeleton for MIDI playback
+ */
+public class MidiViewImpl implements YourViewInterfaceHere {
+  private final Synthesizer synth;
+  private final Receiver receiver;
+
+  public MidiViewImpl() {
+    try {
+      this.synth = MidiSystem.getSynthesizer();
+      this.receiver = synth.getReceiver();
+      this.synth.open();
+    } catch (MidiUnavailableException e) {
+      e.printStackTrace();
+    }
+  }
+  /**
+   * Relevant classes and methods from the javax.sound.midi library:
+   * <ul>
+   *  <li>{@link MidiSystem#getSynthesizer()}</li>
+   *  <li>{@link Synthesizer}
+   *    <ul>
+   *      <li>{@link Synthesizer#open()}</li>
+   *      <li>{@link Synthesizer#getReceiver()}</li>
+   *      <li>{@link Synthesizer#getChannels()}</li>
+   *    </ul>
+   *  </li>
+   *  <li>{@link Receiver}
+   *    <ul>
+   *      <li>{@link Receiver#send(MidiMessage, long)}</li>
+   *      <li>{@link Receiver#close()}</li>
+   *    </ul>
+   *  </li>
+   *  <li>{@link MidiMessage}</li>
+   *  <li>{@link ShortMessage}</li>
+   *  <li>{@link MidiChannel}
+   *    <ul>
+   *      <li>{@link MidiChannel#getProgram()}</li>
+   *      <li>{@link MidiChannel#programChange(int)}</li>
+   *    </ul>
+   *  </li>
+   * </ul>
+   * @see <a href="https://en.wikipedia.org/wiki/General_MIDI">
+   *   https://en.wikipedia.org/wiki/General_MIDI
+   *   </a>
+   */
+
+  public void playNote() throws InvalidMidiDataException {
+    MidiMessage start = new ShortMessage(ShortMessage.NOTE_ON, 0, 60, 64);
+    MidiMessage stop = new ShortMessage(ShortMessage.NOTE_OFF, 0, 60, 64);
+    this.receiver.send(start, -1);
+    this.receiver.send(stop, this.synth.getMicrosecondPosition() + 200000);
+    this.receiver.close(); // Only call this once you're done playing *all* notes
+  }
+}
